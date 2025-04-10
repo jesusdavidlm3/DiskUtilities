32473de57df8de6d35ad81c473241365b903d81d
Jesus David Lozano <jesusdavidlozanomarin@gmail.com>
2025-04-09 23:19:04 -0400

    Manejo de errores y correcciones de tipado.

--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
 import * as fs from "@std/fs";
-import * as cli from "@std/cli"
+// import * as cli from "@std/cli"
 import { promptMultipleSelect } from "@std/cli/unstable-prompt-multiple-select";
 import { detectDrives } from "./functions.ts";
 
 const drives: string[] = await detectDrives()
 let usersPathExist = false
-let origin: string[] = []
-let destiny: string[] = []
+let origin: string[] | null = []
+let destiny: string[] | null = []
 
 
-while(usersPathExist == false || origin.length == 0 || destiny.length == 0) {
-    while(origin.length == 0){
+while(usersPathExist == false || (origin && origin.length == 0) || (destiny && destiny.length == 0)) {
+    while(origin && origin.length == 0){
         origin = promptMultipleSelect("Seleccione un disco de origen", drives, {clear: false})
-        if(origin.length == 0){
+        if(origin && origin.length == 0){
             console.clear()
             console.log("No ha seleccionado ningun disco de origen")
         }
@@ -20,9 +20,9 @@ while(usersPathExist == false || origin.length == 0 || destiny.length == 0) {
     console.clear()
     console.log(`Ah seleccionado el disco ${origin[0]} como origen del respaldo`)
 
-    while(destiny.length == 0){
+    while(destiny && destiny.length == 0){
         destiny = promptMultipleSelect("Seleccione un disco de destino", drives, {clear: false})
-        if(destiny.length == 0){
+        if(destiny && destiny.length == 0){
             console.clear()
             console.log("No ha seleccionado ningun disco de destino")
         }
@@ -35,7 +35,7 @@ while(usersPathExist == false || origin.length == 0 || destiny.length == 0) {
         destiny = []
         console.clear()
         console.log("El directorio de origen no contiene un sistema operativo windows.")
-    }else if(destiny[0] == origin[0]){
+    }else if(destiny && origin && destiny[0] == origin[0]){
         destiny = []
             origin = []
             console.clear()
