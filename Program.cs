c629adb7e21ef84ab1a25c666fcfcb88500964c5
Jesus David Lozano <jesusdavidlozanomarin@gmail.com>
2025-07-19 18:13:43 -0400

    Soporte para windows.old, refactorizadas algunas funciones.

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ string backupName;          //Nombre de la carpeta destino del respaldo
 List<string> errorList = [];
 
 string[] includedDirectories = {    //Carpetas que se copiaran desde todos los usuarios
-    "Documents",
+    // "Documents",
     "Desktop",
     "Downloads",
     "Music",
@@ -24,63 +24,17 @@ List<string> excludedFiles =
 [
     ".exe", ".mp3", ".mp4", ".ini", ".ink", ".tmp", ".lnk", ".log", ".bak", ".old", ".dll", ".win", ".iso",
     ".img", ".asp", ".msp", ".cfg", ".cat", ".bin", ".mkv", ".efi", ".p7b", ".inf", ".msi", ".bmp", ".mui",
-    ".dat", ".man"
+    ".dat", ".man", "m4a"
 ];
-
-void getDrives(){
-    drives = [];
-    foreach(char item in abc)   //Detectamos que unidades existen y las guardamos en la lista de unidades detectadas.
-    {
-        if(Directory.Exists($"{item}:\\"))
-        {
-            drives.Add(item);
-        }
-    }
-}
-
-void copyFiles(string oDirectory, string dDirectory){
-    foreach(string file in Directory.GetFiles(oDirectory))
-    {
-        string fileName = Path.GetFileName(file);
-        string extension = Path.GetExtension(file);
-        if(!excludedFiles.Contains(extension))
-        {
-            Console.WriteLine($"Se esta copiando {file}");
-            File.Copy(file, $"{dDirectory}\\{fileName}", true);
-            Console.WriteLine($"Se copio: {file}");
-        }
-        else
-        {
-            Console.WriteLine($"No se copio: {file}");
-        }
-    }
-}
-
-void copyFolders(string oDirectory, string dDirectory){
-    string folderName = Path.GetFileName(oDirectory);
-    try{
-        Directory.CreateDirectory($"{dDirectory}\\{folderName}");
-        Console.WriteLine($"Se encontro la carpeta: {oDirectory}");
-        copyFiles(oDirectory, $"{dDirectory}\\{folderName}");
-        string[] folders = Directory.GetDirectories(oDirectory);
-        if(folders.Length > 0){
-            foreach(string folder in folders){
-                copyFolders(folder, $"{dDirectory}\\{folderName}");
-            }
-        }
-    }catch(Exception ex){
-        Console.WriteLine("Error al copiar un archivo");
-        errorList.Add(ex.Message);
-    }
-}
 
 for(int i = 0; i < 26;i++)  //Llenamos la lista con todas las letras de posibles unidades
 {
     abc[i] = (char)('A'+i);
 }
 do{             //Este bloque se ejecuta mientras el origen y el destino sean el mismo disco
-    getDrives();
+    GetDrives();
     Console.WriteLine("Seleccione una unidad de origen:");
+    Console.WriteLine("Nota: escriba la letra, no el numero de lista");
     foreach(char item in drives)    //Imprimimos las opciones
     {
         Console.WriteLine($"{listNumber}. Disco {item}");
@@ -102,7 +56,7 @@ do{             //Este bloque se ejecuta mientras el origen y el destino sean el
             Console.WriteLine("Este disco no contiene un sistema operativo Windows");
         }
 
-        getDrives();
+        GetDrives();
         Console.WriteLine("Intente con una de las siguientes opciones:");
         foreach(char item in drives)
         {
@@ -116,9 +70,11 @@ do{             //Este bloque se ejecuta mientras el origen y el destino sean el
     Console.Clear();        
 
     Console.WriteLine($"Ah seleccionado el disco {origin} como disco de origen");
+    // ReSharper disable once StringLiteralTypo
     Console.WriteLine("Seleccione un disco de destino");
+    Console.WriteLine("Nota: escriba la letra, no el numero de lista");
 
-    getDrives();
+    GetDrives();
     foreach(char item in drives)
     {
         Console.WriteLine($"{listNumber}. Disco {item}");
@@ -131,7 +87,7 @@ do{             //Este bloque se ejecuta mientras el origen y el destino sean el
         Console.Clear();
         Console.WriteLine($"La unidad {destiny} no se encuentra en la lista");
         Console.WriteLine("Intente con una de las siguientes opciones:");
-        getDrives();
+        GetDrives();
         foreach(char item in drives)
         {
             Console.WriteLine($"{listNumber}. Disco {item}");
@@ -152,23 +108,21 @@ Console.WriteLine($"Ah seleccionado el disco {origin} como origen y {destiny} co
 string[] excludedDirectories = {    //carpetas predeterminadas de windows que no se respaldaran
     $"{origin}:\\Users\\All Users",
     $"{origin}:\\Users\\Default",
-    $"{origin}:\\Users\\Public",
+    // $"{origin}:\\Users\\Public",
     $"{origin}:\\Users\\Default User"
 };
 
 Console.WriteLine("De un nombre al respaldo");
 backupName = Console.ReadLine();
 
-Console.WriteLine("Escriba las extensiones de archivo que desea excluir separados por espacios");
+Console.WriteLine("Escriba las extensiones de archivo que desea excluir separados por espacios pero incluyendo el punto");
 Console.WriteLine("Por defecto se excluyen: .exe, .mp3, .mp4, .ini, .ink, .tmp, .lnk, .log, .bak, .old, .dll, .win, .ISO, .img, .asp, .MSP, .cfg, .cat, .bin, .mkv, .EFI, .p7b, .inf, .MSI, .BMP, .mui, .dat, .man");
 List<string> rawExcludedFiles = new List<string>(Console.ReadLine().Split(' '));
 foreach(string ext in rawExcludedFiles){
     excludedFiles.Add(ext);
 }
 
-List<string> users = new List<string>(Directory.GetDirectories($"{origin}:\\Users"));   //Obtenemos las capetas de usuario
-
-users = users.Where(i => !excludedDirectories.Contains(i)).ToList();
+List<string> users = GetUsersFolders($"{origin}:");
 
 string destinyDirectory = $"{destiny}:\\{backupName}";  //Carpeta que guaradara el respaldo
 Directory.CreateDirectory(destinyDirectory);     //Creamos el directorio destino del respaldo
@@ -180,9 +134,13 @@ foreach(string user in users)
     Directory.CreateDirectory(destinyUserFolder);
     foreach(string directory in includedDirectories)
     {
-        copyFolders($"{user}\\{directory}", destinyUserFolder);
+        CopyFolders($"{user}\\{directory}", destinyUserFolder);
     }
 }
+
+string oldRoot = $"{origin}:\\windows.old";
+
+CheckOlds(oldRoot);
 
 if(errorList.Count != 0 )
 {
@@ -196,3 +154,82 @@ if(errorList.Count != 0 )
 
 Console.WriteLine("Ejecucion finalizada, presione una tecla para Cerrar");
 Console.ReadLine();
+return;     //Fin de la ejecucion
+
+//Funciones
+void GetDrives(){
+    drives = [];
+    drives.AddRange(abc.Where(item => Directory.Exists($"{item}:\\")));
+}
+
+List<string> GetUsersFolders(string origin)
+{
+    List<string> users = new List<string>(Directory.GetDirectories($"{origin}\\Users"));   //Obtenemos las capetas de usuario
+    users = users.Where(i => !excludedDirectories.Contains(i)).ToList();
+    return users;
+}
+
+void CheckOlds(string oldRoot)
+{
+    if(Directory.Exists(oldRoot))
+    {
+        if (!Directory.Exists($"{destiny}:\\{backupName}\\Instalaciones Anteriores"))
+        {
+            Directory.CreateDirectory($"{destiny}:\\{backupName}\\Instalaciones Anteriores");
+        }
+
+        if (Directory.Exists($"{oldRoot}\\windows.old"))
+        {
+            CheckOlds($"{oldRoot}\\windows.old");
+        }
+        
+        List<string> users = GetUsersFolders($"{oldRoot}");
+
+        foreach (string user in users)
+        {
+            string folderName = Path.GetFileName(user);
+            string destinyFolder = $"{destiny}:\\{backupName}\\Instalaciones Anteriores\\{folderName}";
+            Directory.CreateDirectory(destinyFolder);
+            foreach (string directory in includedDirectories)
+            {
+                CopyFolders($"{oldRoot}\\Users\\{folderName}\\{directory}", destinyFolder);
+            }
+        }
+    }
+}
+
+void CopyFolders(string oDirectory, string dDirectory){
+    string folderName = Path.GetFileName(oDirectory);
+    try{
+        Directory.CreateDirectory($"{dDirectory}\\{folderName}");
+        Console.WriteLine($"Se encontro la carpeta: {oDirectory}");
+        CopyFiles(oDirectory, $"{dDirectory}\\{folderName}");
+        string[] folders = Directory.GetDirectories(oDirectory);
+        if(folders.Length > 0){
+            foreach(string folder in folders){
+                CopyFolders(folder, $"{dDirectory}\\{folderName}");
+            }
+        }
+    }catch(Exception ex){
+        Console.WriteLine("Error al copiar un archivo");
+        errorList.Add(ex.Message);
+    }
+}
+
+void CopyFiles(string oDirectory, string dDirectory){
+    foreach(string file in Directory.GetFiles(oDirectory))
+    {
+        string fileName = Path.GetFileName(file);
+        string extension = Path.GetExtension(file);
+        if(!excludedFiles.Contains(extension))
+        {
+            Console.WriteLine($"Se esta copiando {file}");
+            File.Copy(file, $"{dDirectory}\\{fileName}", true);
+            Console.WriteLine($"Se copio: {file}");
+        }
+        else
+        {
+            Console.WriteLine($"No se copio: {file}");
+        }
+    }
+}
