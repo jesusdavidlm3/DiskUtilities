e3d2c5088500112f835fd65d71a91a188872ceef
Jesus David Lozano <jesusdavidlozanomarin@gmail.com>
2025-07-24 16:34:38 -0400

    Log para errores y windows.old

--- BackupMakerWPF/Logic/Logic.cs
+++ BackupMakerWPF/Logic/Logic.cs
@@ -42,8 +42,20 @@ public class Logic
                 CopyFolders($"{user}\\{directory}", destinyUserFolder, onReport);
             }
         }
-
+        
         string oldRoot = $"{origin}:\\windows.old";
+        CheckOlds(oldRoot, destiny, onReport);
+
+        if (this.errorList.Count > 0)
+        {
+            onReport?.Invoke("Se enconraron los siguientes errores:");
+            foreach(string err in this.errorList)
+            {
+                onReport?.Invoke(err);
+            }
+            onReport?.Invoke("Si desconoce la ubicacion de estas carpetas ignorelas.");
+            onReport?.Invoke("Es posible que sean enlaces simbolicos a otras bibliotecas.");
+        }
     }
 
     private void CopyFiles(string oDirectory, string dDirectory, Action<string> onReport = null)
@@ -54,15 +66,15 @@ public class Logic
             string extension = Path.GetExtension(file);
             if (!excludedFiles.Contains(extension))
             {
-                Console.WriteLine($"Se esta copiando {file}");
+                // Console.WriteLine($"Se esta copiando {file}");
                 onReport?.Invoke($"Se esta copiando {file}");
                 File.Copy(file, $"{dDirectory}\\{fileName}", true);
-                Console.WriteLine($"Se copio: {file}");
+                // Console.WriteLine($"Se copio: {file}");
                 onReport?.Invoke($"Se copio: {file}");
             }
             else
             {
-                Console.WriteLine($"No se copio: {file}");
+                // Console.WriteLine($"No se copio: {file}");
                 onReport?.Invoke($"No se copio: {file}");
             }
         }
@@ -74,7 +86,7 @@ public class Logic
         try
         {
             Directory.CreateDirectory($"{dDirectory}\\{folderName}");
-            Console.WriteLine($"Se encontro la carpeta: {oDirectory}");
+            // Console.WriteLine($"Se encontro la carpeta: {oDirectory}");
             onReport?.Invoke($"Se encontro la carpeta: {oDirectory}");
             CopyFiles(oDirectory, $"{dDirectory}\\{folderName}", onReport);
             string[] folders = Directory.GetDirectories(oDirectory);
@@ -88,7 +100,7 @@ public class Logic
         }
         catch (Exception ex)
         {
-            Console.WriteLine("Error al copiar un archivo");
+            // Console.WriteLine("Error al copiar un archivo");
             errorList.Add(ex.Message);
         }
     }
@@ -97,12 +109,41 @@ public class Logic
     {
         this.excludedDirectories =
         [
-            $"{oDirectory}:\\Users\\All Users",
-            $"{oDirectory}:\\Users\\Default",
-            $"{oDirectory}:\\Users\\Default User"
+            $"{oDirectory}\\Users\\All Users",
+            $"{oDirectory}\\Users\\Default",
+            $"{oDirectory}\\Users\\Default User"
         ];
         List<string> users = new List<string>(Directory.GetDirectories($"{oDirectory}\\Users"));   //Obtenemos las capetas de usuario
         users = users.Where(i => !excludedDirectories.Contains(i)).ToList();
         return users;
+    }
+
+    private void CheckOlds(string oldRoot, string destiny, Action<string> onReport = null)
+    {
+        if(Directory.Exists(oldRoot))
+        {
+            if (!Directory.Exists($"{destiny}:\\Nuevo respaldo\\Instalaciones Anteriores"))
+            {
+                Directory.CreateDirectory($"{destiny}:\\Nuevo respaldo\\Instalaciones Anteriores");
+            }
+
+            if (Directory.Exists($"{oldRoot}\\windows.old"))
+            {
+                CheckOlds($"{oldRoot}\\windows.old", destiny, onReport);
+            }
+        
+            List<string> users = GetUsersFolders($"{oldRoot}");
+
+            foreach (string user in users)
+            {
+                string folderName = Path.GetFileName(user);
+                string destinyFolder = $"{destiny}:\\Nuevo respaldo\\Instalaciones Anteriores\\{folderName}";
+                Directory.CreateDirectory(destinyFolder);
+                foreach (string directory in includedDirectories)
+                {
+                    CopyFolders($"{oldRoot}\\Users\\{folderName}\\{directory}", destinyFolder, onReport);
+                }
+            }
+        }
     }
 }
