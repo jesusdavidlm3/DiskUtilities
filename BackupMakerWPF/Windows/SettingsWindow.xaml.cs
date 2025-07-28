f31faa5788a8240c5c0501e2230c3f2d2ef530da
Jesus David Lozano <jesusdavidlozanomarin@gmail.com>
2025-07-27 20:37:06 -0400

    Persistencia en memoria de seleccion de extensiones.

--- BackupMakerWPF/Windows/SettingsWindow.xaml.cs
+++ BackupMakerWPF/Windows/SettingsWindow.xaml.cs
@@ -27,18 +27,19 @@ public class Extension
 
 public class VideoExtensions : ObservableCollection<Extension>
 {
+    
+    public static List<Extension> extensions = new List<Extension>
+    {
+        new (".mp4", false),
+        new (".mkv", false),
+        new (".avi", false),
+        new (".wmv", false),
+        new (".mov", false),
+        new (".webm", false),
+    };
+    
     public VideoExtensions()
     {
-        var extensions = new List<Extension>()
-        {
-            new (".mp4", false),
-            new (".mkv", false),
-            new (".avi", false),
-            new (".wmv", false),
-            new (".mov", false),
-            new (".webm", false),
-        };
-
         foreach (var extension in extensions)
         {
             Add(extension);
@@ -48,17 +49,17 @@ public class VideoExtensions : ObservableCollection<Extension>
 
 class AudioExtensions : ObservableCollection<Extension>
 {
+
+    public static List<Extension> extensions = new List<Extension>{
+        new (".mp3", false),
+        new (".m4a", false),
+        new (".wav", false),
+        new (".aac", false),
+        new (".flac", false),
+    };
+    
     public AudioExtensions()
     {
-        var extensions = new List<Extension>()
-        {
-            new (".mp3", false),
-            new (".m4a", false),
-            new (".wav", false),
-            new (".aac", false),
-            new (".flac", false),
-        };
-        
         foreach (var extension in extensions)
         {
             Add(extension);
@@ -68,18 +69,18 @@ class AudioExtensions : ObservableCollection<Extension>
 
 public class ImageExtensions : ObservableCollection<Extension>
 {
+    public static List<Extension> extensions = new List<Extension>
+    {
+        new(".bmp", false),
+        new(".gif", false),
+        new(".jpg", false),
+        new(".jpeg", false),
+        new(".png", false),
+        new(".tiff", false),
+        new(".webp", false),
+    };
     public ImageExtensions()
     {
-        var extensions = new List<Extension>
-        {
-            new(".bmp", false),
-            new(".gif", false),
-            new(".jpg", false),
-            new(".jpeg", false),
-            new(".png", false),
-            new(".tiff", false),
-            new(".webp", false),
-        };
         foreach (var extension in extensions)
         {
             Add(extension);
@@ -87,30 +88,30 @@ public class ImageExtensions : ObservableCollection<Extension>
     }
 }
 
-class DocExtensions : ObservableCollection<Extension>
+public class DocExtensions : ObservableCollection<Extension>
 {
+    public static List<Extension> extensions = new List<Extension>()
+    {
+        new(".txt", false),
+        new(".pdf", false),
+        new(".doc", false),
+        new(".docx", false),
+        new(".xls", false),
+        new(".xlsx", false),
+        new(".ppt", false),
+        new(".pptx", false),
+        new(".pptm", false),
+        new(".pptxm", false),
+        new(".odt", false),
+        new(".odtx", false),
+        new(".odsx", false),
+        new(".odsxm", false),
+        new(".odtm", false),
+        new(".odtmx", false),
+        new(".odtxm", false),
+    };
     public DocExtensions()
     {
-        var extensions = new List<Extension>()
-        {
-            new(".pdf", false),
-            new(".doc", false),
-            new(".docx", false),
-            new(".xls", false),
-            new(".xlsx", false),
-            new(".ppt", false),
-            new(".pptx", false),
-            new(".pptm", false),
-            new(".pptxm", false),
-            new(".odt", false),
-            new(".odtx", false),
-            new(".odsx", false),
-            new(".odsxm", false),
-            new(".odtm", false),
-            new(".odtmx", false),
-            new(".odtxm", false),
-        };
-        
         foreach (var extension in extensions)
         {
             Add(extension);
@@ -120,17 +121,18 @@ class DocExtensions : ObservableCollection<Extension>
 
 class CompressedExtensions : ObservableCollection<Extension>
 {
+
+    public static List<Extension> extensions = new List<Extension>
+    {
+        new (".iso", false),
+        new (".zip", false),
+        new (".rar", false),
+        new (".7z", false),
+        new (".tar.gz", false)
+    };
+    
     public CompressedExtensions()
     {
-        var extensions = new List<Extension>()
-        {
-            new (".iso", false),
-            new (".zip", false),
-            new (".rar", false),
-            new (".7z", false),
-            new (".tar.gz", false)
-        };
-        
         foreach (var extension in extensions)
         {
             Add(extension);
@@ -140,34 +142,36 @@ class CompressedExtensions : ObservableCollection<Extension>
 
 class SystemExtensions : ObservableCollection<Extension>
 {
+    
+    public static List<Extension> extensions = new List<Extension>
+    {
+        new(".exe", false),
+        new(".ini", true),
+        new(".ink", true),
+        new(".tmp", true),
+        new(".lnk", true),
+        new(".log", true),
+        new(".bak", true),
+        new(".old", true),
+        new(".dll", true),
+        new(".win", true),
+        new(".img", true),
+        new(".asp", true),
+        new(".msp", true),
+        new(".cfg", true),
+        new(".cat", true),
+        new(".bin", true),
+        new(".efi", true),
+        new(".p7b", true),
+        new(".inf", true),
+        new(".msi", true),
+        new(".mui", true),
+        new(".dat", true),
+        new(".man", true)
+    };
+    
     public SystemExtensions()
     {
-        var extensions = new List<Extension>()
-        {
-            new(".exe", false),
-            new(".ini", true),
-            new(".ink", true),
-            new(".tmp", true),
-            new(".lnk", true),
-            new(".log", true),
-            new(".bak", true),
-            new(".old", true),
-            new(".dll", true),
-            new(".win", true),
-            new(".img", true),
-            new(".asp", true),
-            new(".msp", true),
-            new(".cfg", true),
-            new(".cat", true),
-            new(".bin", true),
-            new(".efi", true),
-            new(".p7b", true),
-            new(".inf", true),
-            new(".msi", true),
-            new(".mui", true),
-            new(".dat", true),
-            new(".man", true)
-        };
         foreach (var extension in extensions)
         {
             Add(extension);
@@ -177,25 +181,27 @@ class SystemExtensions : ObservableCollection<Extension>
 
 public class ProjectExtensions : ObservableCollection<Extension>
 {
+
+    public static List<Extension> extensions = new List<Extension>
+    {
+        new(".psd", false),
+        new(".ai", false),
+        new(".indd", false),
+        new(".poproj", false),
+        new(".chproj", false),
+        new(".blend", false),
+        new(".drp", false),
+        new(".dra", false),
+        new(".settings", false),
+        new(".obj", false),
+        new(".fbx", false),
+        new(".stl", false),
+        new(".ply", false),
+        new(".dae", false),
+    };
+    
     public ProjectExtensions()
     {
-        var extensions = new List<Extension>
-        {
-            new(".psd", false),
-            new (".ai", false),
-            new (".indd", false),
-            new (".poproj", false),
-            new (".chproj", false),
-            new (".blend", false),
-            new (".drp", false),
-            new (".dra", false),
-            new (".settings", false),
-            new (".obj", false),
-            new (".fbx", false),
-            new (".stl", false),
-            new (".ply", false),
-            new (".dae", false),
-        };
         foreach (var extension in extensions)
         {
             Add(extension);
